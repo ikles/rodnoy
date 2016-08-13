3f255b2863bb40f4c13d672d778c7d92890cf002
Igor Kleshev <membship@ya.ru>
2016-08-13 19:33:10 +0700

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -5,4 +5,19 @@ $(function() {
 		$(".main-mnu").slideToggle();
 		return false;
 	});
+
+	$(".view-all-reviews").click(function(event) {
+		event.preventDefault();		
+
+		if ($('.reviews-cantainer').hasClass('reviews-cantainer-full')) {
+			$('.reviews-cantainer').removeClass('reviews-cantainer-full');
+			$(".view-all-reviews").html('Посмотреть все отзывы');
+		}
+		else {
+			$('.reviews-cantainer').addClass('reviews-cantainer-full');	
+			$(".view-all-reviews").html('Скрыть');
+		}
+	});
+	
+
 });
